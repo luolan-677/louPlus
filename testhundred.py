2ad6addf4950a4e1b54546389713534af87452a3
luo <770713099@qq.com>
2020-02-07 02:59:45 +0800

    compared with 100

---
+++ testhundred.py
@@ -0,0 +1,6 @@
+#! /usr/bin/env python3
+number = int(input("please enter a number: "))
+if number < 100:
+    print("your number is less than 100")
+else:
+    print("your number is greater than or equal to 100")
