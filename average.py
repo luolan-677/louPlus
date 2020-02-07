8fc36a378f7bef029445a2a5eb46ead0f200be4e
luo <770713099@qq.com>
2020-02-07 14:16:05 +0800

    计算平均数

---
+++ average.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+N = int(input("please enter a number:"))
+sum = 0
+count = 0
+print("please input {} numbers:".format(N))
+while count < N:
+    number = float(input())
+    sum = sum + number
+    count = count + 1
+average = sum / N
+print("N = {}, sum = {}".format(N,sum))
+print("average = {:.2f}".format(average))
